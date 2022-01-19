bdc476e60ba62976a7a66fe09d1f616ef079df3f
shilat2132 <shilat2132@gmail.com>
2022-01-19 21:12:28 +0200

    i

--- app.js
+++ app.js
@@ -117,186 +117,7 @@ app.delete("/deletestory/:id", function(req,res){
 
 })
 
-//lyrics
-var song = new mon.Schema({
-  name: String,
-  singer: String,
-  lyrics: String
-})
 
-var Song = mon.model("Song", song)
-
-
-// SONG create - get and post
-app.get("/createsong", function(req, res){
-  res.render("songs/createsong")
-})
-
-app.post("/songcr", function(req, res){
-  var newSong = req.body.song
-  Song.create(newSong, function(err, ne){
-    if(err){
-      res.send("error")
-    }
-    else{
-      res.redirect("/indexsongs")
-    }
-  })
-})
-
-//SONG index
-app.get("/indexsongs", function(req,res){
-  Song.find({}, function(err, allsongs){
-    if(err){
-      res.send("error")
-    }
-    else{
-      res.render("songs/indexsongs", {allsongs: allsongs})
-    }
-  })
-})
-
-//SONG - show
-app.get("/showsong/:id", function(req, res){
-  var songID = req.params.id
-  Song.findById(songID, function(err, showedsong){
-    if(err){
-      res.send("err")
-    }
-    else{
-      res.render("songs/showsong", {showedsong: showedsong})
-    }
-  })
-})
-
-//SONG - edit
-app.get("/editsong/:id", function(req,res){
-  Song.findById(req.params.id, function(err, editit){
-    if(err){
-      res.send("err")
-    }
-    else{
-      res.render("songs/editsong", {editit: editit})
-    }
-  })
-
-
-})
-
-//SONG update
-app.put("/updatesong/:id", function(req, res){
-  var updatedSong = req.body.song
-  Song.findByIdAndUpdate(req.params.id, updatedSong, function(err, updated){
-    if(err){
-      res.send("err")
-    } else{
-      res.redirect("/indexsongs")
-    }
-  })
-})
-
-//SONG delete
-app.delete("/deletesong/:id", function(req, res){
-  Song.findByIdAndRemove(req.params.id, function(err){
-    if (err){
-      res.send("err")
-    } else{
-      res.redirect("/indexsongs")
-    }
-  })
-})
-
-//מתכונים
-var fo = new mon.Schema ({
-  name: String,
-  state: String,
-  catagory: String,
-  ingridiants: String,
-  making: String
-})
-
-var Food = mon.model("Food", fo)
-
-//FOOD create routes - get amd post
-app.get("/foodCreate", function(req,res){
-  res.render("foods/createFoods")
-})
-
-app.post("/foodCr", function(req, res){
-  var newFood = req.body.food
-  Food.create(newFood, function(err, ne){
-    if(err){
-      res.send ("error")
-    }
-    else{
-      res.redirect("/indexFoods")
-    }
-  })
-})
-
-//FOOD delete
-app.delete("/deleteFood/:id", function(req,res){
-  var id = req.params.id
-  Food.findByIdAndDelete(id, function(err){
-    if(err){
-      res.send("error")
-    }
-    else{
-      res.redirect("/indexFoods")
-    }
-  })
-})
-//FOOD index
-app.get("/indexFoods", function(req,res){
-  Food.find({}, function(err, arrIndex){
-    if (err){
-      res.send("error")
-    }
-    else{
-      res.render("foods/indexFoods", {arr: arrIndex})
-    }
-  })
-})
-
-//FOOD edit
-app.get("/editFood/:id", function(req,res){
-  var id = req.params.id
-  Food.findById(id, function(err, editF){
-    if(err){
-      res.send("error")
-    }
-    else{
-      res.render("foods/editFood", {editF: editF})
-    }
-  })
-})
-
-//FOOD update
-app.put("/updateFood/:id", function(req,res){
-  var id = req.params.id
-  Food.findByIdAndUpdate(id, req.body.food, function(err, updated){
-    if(err){
-      res.send("error")
-    }
-    else{
-      res.redirect("/indexFoods")
-    }
-  })
-})
-
-
-//FOOD show page 
-app.get("/catagory/:id", function(req,res){
-  var id = req.params.id
-  Food.findById(id, function(err, foundFood){
-    if (err){
-      res.send("error in the show page")
-    }
-    else{
-      res.render("foods/showFood", {currentFood: foundFood})
-    }
-  })
-})
 
 // BOOK collection
 var hand = new mon.Schema({
@@ -407,6 +228,242 @@ app.post("/searchbook", function(req, res){
   })
 
 })
+
+//lyrics
+var song = new mon.Schema({
+  name: String,
+  singer: String,
+  lyrics: String,
+  series: Boolean
+})
+
+var Song = mon.model("Song", song)
+
+
+// SONG create - get and post
+app.get("/createsong", function(req, res){
+  res.render("songs/createsong")
+})
+
+app.post("/songcr", function(req, res){
+  var newSong = req.body.song
+  Song.create(newSong, function(err, ne){
+    if(err){
+      res.send("error")
+    }
+    else{
+      res.redirect("/indexsongs")
+    }
+  })
+})
+
+//SONG index
+app.get("/indexsongs", function(req,res){
+  Song.find({}, function(err, allsongs){
+    if(err){
+      res.send("error")
+    }
+    else{
+      res.render("songs/indexsongs", {allsongs: allsongs})
+    }
+  })
+})
+
+//SONG - show
+app.get("/showsong/:id", function(req,res){
+  Song.findById(req.params.id, function(err1, showedsong){
+    if(showedsong.series){
+     Song.find({singer: showedsong.singer}, function(err2, seriessongs){
+       if((err1)||(err2)){
+         res.send("err")
+       }else{
+        res.render("songs/showsong", {showedsong: showedsong, seriessongs:seriessongs})
+      }
+     })
+    }
+    else{
+      if(err1){
+        res.send("err")
+      }else{
+        res.render("songs/showsong", {showedsong: showedsong})
+      }
+    }
+  })
+})
+
+
+//SONG - edit
+app.get("/editsong/:id", function(req,res){
+  Song.findById(req.params.id, function(err, editit){
+    if(err){
+      res.send("err")
+    }
+    else{
+      res.render("songs/editsong", {editit: editit})
+    }
+  })
+
+
+})
+
+
+
+//SONG update
+app.put("/updatesong/:id", function(req, res){
+  var updatedSong = req.body.song
+  Song.findByIdAndUpdate(req.params.id, updatedSong, function(err, updated){
+    if(err){
+      res.send("err")
+    } else{
+      res.redirect("/indexsongs")
+    }
+  })
+})
+
+//SONG delete
+app.delete("/deletesong/:id", function(req, res){
+  Song.findByIdAndRemove(req.params.id, function(err){
+    if (err){
+      res.send("err")
+    } else{
+      res.redirect("/indexsongs")
+    }
+  })
+})
+
+// SONGS SEARCH
+app.post("/searchsong", function(req, res){
+  var searchinput = req.body.searchinput
+  var songssresults = []
+  Song.find({}, function(err, searchsongs){
+    if (err){
+      res.send("err")
+    } else{
+      searchsongs.forEach(function(song){
+        if(song.name.includes(searchinput) || song.singer.includes(searchinput) ){
+          songssresults.push(song)
+        }
+      })
+      res.render("songs/searchpage", {searchinput: searchinput, songssresults:songssresults})
+    }
+  })
+
+})
+
+//מתכונים
+var fo = new mon.Schema ({
+  name: String,
+  img: String,
+  state: String,
+  catagory: String,
+  ingridiants: String,
+  making: String
+})
+
+var Food = mon.model("Food", fo)
+
+//FOOD create routes - get amd post
+app.get("/foodCreate", function(req,res){
+  res.render("foods/createFoods")
+})
+
+app.post("/foodCr", function(req, res){
+  var newFood = req.body.food
+  Food.create(newFood, function(err, ne){
+    if(err){
+      res.send ("error")
+    }
+    else{
+      res.redirect("/צהריים")
+    }
+  })
+})
+
+//FOOD delete
+app.delete("/deleteFood/:id", function(req,res){
+  var id = req.params.id
+  Food.findByIdAndDelete(id, function(err){
+    if(err){
+      res.send("error")
+    }
+    else{
+      res.redirect("/צהריים")
+    }
+  })
+})
+//FOOD index
+app.get("/:indexFoods", function(req,res){
+  var indexfoods = req.params.indexFoods
+  Food.find({catagory:indexfoods }, function(err, foods){
+    if (err){
+      res.send("error")
+    }
+    else{
+      res.render("foods/indexFoods", {foods: foods, indexfoods:indexfoods })
+    }
+  })
+})
+
+//FOOD edit
+app.get("/editFood/:id", function(req,res){
+  var id = req.params.id
+  Food.findById(id, function(err, editF){
+    if(err){
+      res.send("error")
+    }
+    else{
+      res.render("foods/editFood", {editF: editF})
+    }
+  })
+})
+
+//FOOD update
+app.put("/updateFood/:id", function(req,res){
+  var id = req.params.id
+  Food.findByIdAndUpdate(id, req.body.food, function(err, updated){
+    if(err){
+      res.send("error")
+    }
+    else{
+      res.redirect("/צהריים")
+    }
+  })
+})
+
+
+//FOOD show page 
+app.get("/showfood/:id", function(req,res){
+  var id = req.params.id
+  Food.findById(id, function(err, showedfood){
+    if (err){
+      res.send("error in the show page")
+    }
+    else{
+      res.render("foods/showFood", {showedfood: showedfood})
+    }
+  })
+})
+
+// FOODS SEARCH
+app.post("/searchfood", function(req, res){
+  var searchinput = req.body.searchinput
+  var foodsresults = []
+  Food.find({}, function(err, searchfoods){
+    if (err){
+      res.send("err")
+    } else{
+      searchfoods.forEach(function(food){
+        if(food.name.includes(searchinput) ){
+          foodsresults.push(food)
+        }
+      })
+      res.render("foods/searchpage", {searchinput: searchinput, foodsresults:foodsresults})
+    }
+  })
+
+})
+
+
 
 
 
